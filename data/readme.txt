bcd4c91657914cf2b1504c1ddf5858e36a599f04
James Hobin <hobinjk@mit.edu>
2017-04-10 17:09:32 -0400

    Add data directory; Switch to 3D rendering

---
+++ data/readme.txt
@@ -0,0 +1,2 @@
+graciously provided by /u/mncke
+see https://www.reddit.com/r/place/comments/6396u5/rplace_archive_update/ for source
--- place-webgl.js
+++ place-webgl.js
@@ -1,26 +1,35 @@
-var req = new XMLHttpRequest();
-req.open('GET', 'diffs.bin');
-req.responseType = 'arraybuffer';
+function loadPlaceData() {
+  return new Promise(function(resolve, reject) {
+    var req = new XMLHttpRequest();
+    req.open('GET', 'data/diffs.bin');
+    req.responseType = 'arraybuffer';
 
-var placeData;
+    req.onload = function() {
+      if (req.response) {
+        // Assume platform is little-endian
+        resolve(new Uint32Array(req.response));
+      }
+    };
 
-req.onload = function() {
-  if (req.response) {
-    // Assume platform is little-endian
-    placeData = new Uint32Array(req.response);
-    placeIndex = 0;
-    update();
-  }
-};
+    req.onerror = function(e) {
+      reject(e);
+    };
 
-function update() {
-  for (var i = 0; i < 1000; i++) {
-    playbackOne();
-  }
-  window.requestAnimationFrame(update);
+    req.send();
+  });
 }
 
-var colorCodes = [
+var renderer;
+
+loadPlaceData().then(function(placeData) {
+  renderer = new Renderer(placeData);
+  while (renderer.time <= startTime) {
+    renderer.playbackOne();
+  }
+  renderer.update();
+});
+
+const colorCodes = [
   0xFF, 0xFF, 0xFF,
   0xE4, 0xE4, 0xE4,
   0x88, 0x88, 0x88,
@@ -38,33 +47,88 @@ var colorCodes = [
   0xCF, 0x6E, 0xE4,
   0x82, 0x00, 0x80
 ];
+const placeSize = 1000;
+const startTime = 1490986860;
 
-var canvas = document.getElementById('place');
-canvas.width = 1000;
-canvas.height = 1000;
-var gfx = canvas.getContext('2d');
-
-var pixelImageData = gfx.createImageData(1, 1);
-var lastTime = -1;
-
-function playbackOne() {
-  var time = placeData[4 * placeIndex + 0];
-  if (time < lastTime) {
-    console.log('linearity violation', time, lastTime);
-  }
-  lastTime = time;
-  var x =  placeData[4 * placeIndex + 1];
-  var y = placeData[4 * placeIndex + 2];
-  var colorCode = placeData[4 * placeIndex + 3];
-  var color = colorCodes[colorCode];
-  pixelImageData.data[0] = colorCodes[colorCode * 3 + 0];
-  pixelImageData.data[1] = colorCodes[colorCode * 3 + 1];
-  pixelImageData.data[2] = colorCodes[colorCode * 3 + 2];
-  pixelImageData.data[3] = 255;
-  gfx.putImageData(pixelImageData, x, y);
-
-  placeIndex += 1;
+function Renderer(placeData) {
+  this.placeData = placeData;
+  this.placeIndex = 0;
+  this.time = this.placeData[0];
+  this.update = this.update.bind(this);
+  this.initThree();
+  window.requestAnimationFrame(this.update);
 }
 
+Renderer.prototype.initThree = function() {
+	this.scene = new THREE.Scene();
 
-req.send();
+	this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+	this.camera.position.z = 500;
+  var size = 1000;
+  this.geometry = new THREE.PlaneBufferGeometry(size, size, placeSize - 1, placeSize - 1);
+  this.colors = new Uint8Array(placeSize * placeSize * 3);
+
+  for (var i = 0; i < this.colors.length; i++) {
+    this.colors[i] = 255;
+  }
+
+  this.colorTexture = new THREE.DataTexture(this.colors, placeSize, placeSize, THREE.RGBFormat, THREE.UnsignedByteType);
+  this.colorTexture.needsUpdate = true;
+
+  // this.geometry.addAttribute('color', new THREE.BufferAttribute(this.colors, 3, true));
+  var material = new THREE.MeshBasicMaterial({side: THREE.DoubleSide, map: this.colorTexture});
+  this.plane = new THREE.Mesh(this.geometry, material);
+  this.scene.add(this.plane);
+
+	this.renderer = new THREE.WebGLRenderer();
+	this.renderer.setSize(window.innerWidth, window.innerHeight);
+
+  var controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
+  controls.enableZoom = true;
+
+	document.body.appendChild(this.renderer.domElement);
+};
+
+Renderer.prototype.update = function() {
+  for (var i = 0; i < 1000; i++) {
+    this.playbackOne();
+  }
+  this.smoothPositions();
+  this.renderer.render(this.scene, this.camera);
+  window.requestAnimationFrame(this.update);
+};
+
+var decayFactor = 0.99;
+var upFactor = 1;
+
+Renderer.prototype.smoothPositions = function() {
+  var positionArray = this.geometry.attributes.position.array;
+  for (var i = 0; i < positionArray.length / 3; i++) {
+    positionArray[i * 3 + 2] *= decayFactor;
+  }
+};
+
+Renderer.prototype.playbackOne = function() {
+  var baseIndex = 4 * this.placeIndex;
+  var time = this.placeData[baseIndex + 0];
+  if (time < this.time) {
+    console.log('linearity violation', time, this.time);
+  }
+  this.time = time;
+  var x =  this.placeData[baseIndex + 1];
+  var y = this.placeData[baseIndex + 2];
+  var colorCode = 3 * this.placeData[baseIndex + 3];
+  var colorIndex = 3 * ((placeSize - y - 1) * placeSize + x);
+  this.colors[colorIndex + 0] = colorCodes[colorCode + 0];
+  this.colors[colorIndex + 1] = colorCodes[colorCode + 1];
+  this.colors[colorIndex + 2] = colorCodes[colorCode + 2];
+  this.colorTexture.needsUpdate = true;
+
+  var attributeIndex = 3 * (y * placeSize + x)
+  var positionArray = this.geometry.attributes.position.array;
+  positionArray[attributeIndex + 2] += upFactor;
+  this.geometry.attributes.position.needsUpdate = true;
+
+  this.placeIndex += 1;
+};
+
